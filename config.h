fb405c74d66bc2734e552c3795fa0b44b247d13d
Simon Marache-Francisco <simon.marache@kirontech.com>
2020-06-23 14:07:06 +0100

    Add solarized colors

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int borderpx = 2;
  * 5: value of shell in config.h
  */
 static char *shell = "/usr/bin/fish";
-char *tmux = "/usr/bin/tmux";
+char *tmux = "tmux";
 
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
@@ -91,38 +91,40 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
-
 unsigned int paletteindex = 0;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorpalettes[2][258] = {
 	{
-		/* 8 normal colors */
-		"black",
-		"red3",
-		"green3",
-		"yellow3",
-		"blue2",
-		"magenta3",
-		"cyan3",
-		"gray90",
+        /* 8 normal colors */
+        "#073642",
+        "#dc322f",
+        "#859900",
+        "#b58900",
+        "#268bd2",
+        "#d33682",
+        "#2aa198",
+        "#eee8d5",
 
-		/* 8 bright colors */
-		"gray50",
-		"red",
-		"green",
-		"yellow",
-		"#5c5cff",
-		"magenta",
-		"cyan",
-		"white",
+        /* 8 bright colors */
+        "#002b36",
+        "#cb4b16",
+        "#586e75",
+        "#657b83",
+        "#839496",
+        "#6c71c4",
+        "#93a1a1",
+        "#fdf6e3",
 
-		[255] = 0,
+        [255] = 0,
 
-		/* more colors can be added after 255 to use with DefaultXX */
-		"#cccccc",
-		"#555555",
+        /* more colors can be added after 255 to use with DefaultXX */
+        "#002b36",
+        "#839496",
+        "#002b36",
+        "#839496",
 	}, {
 		/* 8 normal colors */
+		"black",
 		"red3",
 		"green3",
 		"yellow3",
@@ -130,23 +132,24 @@ static const char *colorpalettes[2][258] = {
 		"magenta3",
 		"cyan3",
 		"gray90",
-		"black",
 
 		/* 8 bright colors */
 		"gray50",
+		"red",
 		"green",
 		"yellow",
 		"#5c5cff",
 		"magenta",
 		"cyan",
 		"white",
-		"red",
 
 		[255] = 0,
 
 		/* more colors can be added after 255 to use with DefaultXX */
-		"#cccccc",
+		"black",
+		"gray90",
 		"#555555",
+		"#cccccc",
 	}
 };
 
@@ -155,10 +158,10 @@ static const char *colorpalettes[2][258] = {
  * Default colors (colorpalettes index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+static unsigned int defaultrcs = 258;
+static unsigned int defaultcs = 259;
 
 /*
  * Default shape of cursor
