ad0f37b0443f46261f296085b89d4c4107e2e878
Simon M <git@simon.marache.net>
2022-10-03 22:55:14 +0100

    Fix file not uploaded after merge

--- config.h
+++ config.h
@@ -159,7 +159,7 @@ static const char *colorpalettes[2][260] = {
 unsigned int defaultbg = 256;
 unsigned int defaultfg = 257;
 static unsigned int defaultrcs = 258;
-static unsigned int defaultcs = 259;
+unsigned int defaultcs = 259;
 
 /*
  * Default shape of cursor
